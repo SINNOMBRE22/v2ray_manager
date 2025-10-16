fc439a747f87b7fbfab9bcdd8b4685498901d2c8
Cristian Aguilar <107384946+SINNOMBRE22@users.noreply.github.com>
2025-10-16 03:24:28 -0600

    Update install-v2r.sh

--- install-v2r.sh
+++ install-v2r.sh
@@ -1,149 +1,91 @@
 #!/bin/bash
+set -e
 
 BARRA="\033[1;36m-----------------------------------------------------\033[0m"
-IVAR="/etc/http-instas"
-SCPT_DIR="/etc/SCRIPT"
-SCPinstal="$HOME/install"
-rm $(pwd)/$0
+PACKAGES=(bc jq curl npm nodejs socat netcat netcat-traditional net-tools cowsay figlet lolcat)
+PAQUETES_FAILED=()
 
-add-apt-repository universe
-apt update -y; apt upgrade -y
-
-install_ini () {
-clear
-echo -e "$BARRA"
-echo -e "\033[92m        -- INSTALANDO PAQUETES NECESARIOS -- "
-echo -e "$BARRA"
-#bc
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install bc................... $ESTATUS "
-#jq
-[[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || apt-get install jq -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install jq................... $ESTATUS "
-#curl
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install curl................. $ESTATUS "
-#npm
-[[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] || apt-get install npm -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install npm.................. $ESTATUS "
-#nodejs
-[[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] || apt-get install nodejs -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install nodejs............... $ESTATUS "
-#socat
-[[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] || apt-get install socat -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install socat................ $ESTATUS "
-#netcat
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install netcat............... $ESTATUS "
-#netcat-traditional
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || apt-get install netcat-traditional -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install netcat-traditional... $ESTATUS "
-#net-tools
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || apt-get install net-tools -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install net-tools............ $ESTATUS "
-#cowsay
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || apt-get install cowsay -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install cowsay............... $ESTATUS "
-#figlet
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install figlet............... $ESTATUS "
-#lolcat
-apt-get install lolcat -y &>/dev/null
-sudo gem install lolcat &>/dev/null
-[[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m  # apt-get install lolcat............... $ESTATUS "
-
-echo -e "$BARRA"
-echo -e "\033[92m La instalación de paquetes necesarios ha finalizado"
-echo -e "$BARRA"
-echo -e "\033[97m Si la instalación de paquetes tiene fallas"
-echo -ne "\033[97m Puede intentar de nuevo [s/n]: "
-read inst
-[[ $inst = @(s|S|y|Y) ]] && install_ini
-}
+# Verifica permisos de root
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\033[91mPor favor ejecuta este script como root (sudo).\033[0m"
+    exit 1
+fi
 
 msg () {
-BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
-AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCOR='\e[0m'
- case $1 in
-  -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
-  -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -verm)cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -bra)cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}";;
-  "-bar2"|"-bar")cor="${VERMELHO}======================================================" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
- esac
+    case $1 in
+      -ne) echo -ne "\e[31m\e[1m${2}\e[0m";;
+      -ama) echo -e "\e[33m\e[1m${2}\e[0m";;
+      -verm) echo -e "\e[33m\e[1m[!] \e[31m${2}\e[0m";;
+      -azu) echo -e "\033[1;36m\e[1m${2}\e[0m";;
+      -verd) echo -e "\e[32m\e[1m${2}\e[0m";;
+      -bra) echo -ne "\e[31m${2}\e[0m";;
+      "-bar2"|"-bar") echo -e "\e[31m======================================================\e[0m";;
+    esac
 }
 
-ofus () {
-unset server
-server=$(echo ${txt_ofuscatw}|cut -d':' -f1)
-unset txtofus
-number=$(expr length $1)
-for((i=1; i<$number+1; i++)); do
-txt[$i]=$(echo "$1" | cut -b $i)
-case ${txt[$i]} in
-".")txt[$i]="*";;
-"*")txt[$i]=".";;
-"1")txt[$i]="@";;
-"@")txt[$i]="1";;
-"2")txt[$i]="?";;
-"?")txt[$i]="2";;
-"4")txt[$i]="%";;
-"%")txt[$i]="4";;
-"-")txt[$i]="K";;
-"K")txt[$i]="-";;
-esac
-txtofus+="${txt[$i]}"
-done
-echo "$txtofus" | rev
-}
+install_packages () {
+    clear
+    echo -e "$BARRA"
+    echo -e "\033[92m        -- INSTALANDO PAQUETES NECESARIOS -- "
+    echo -e "$BARRA"
 
-verificar_arq () {
-unset ARQ
-case $1 in
-"v2r.sh")ARQ="/usr/bin/";;
-esac
-mv -f ${SCPinstal}/$1 ${ARQ}/$1
-chmod +x ${ARQ}/$1
-}
+    # Agrega repositorio universe si no está
+    if ! grep -q "^deb .*universe" /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
+        add-apt-repository universe -y
+    fi
 
-meu_ip () {
-MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-MIP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+    apt update -y && apt upgrade -y
+
+    for pkg in "${PACKAGES[@]}"; do
+        if ! dpkg -s "$pkg" &>/dev/null; then
+            apt-get install "$pkg" -y &>/dev/null
+        fi
+        if dpkg -s "$pkg" &>/dev/null; then
+            ESTATUS="\033[92mINSTALADO"
+        else
+            ESTATUS="\033[91mFALLO DE INSTALACION"
+            PAQUETES_FAILED+=("$pkg")
+        fi
+        echo -e "\033[97m  # apt-get install $pkg................... $ESTATUS \033[0m"
+    done
+
+    # lolcat: intenta instalar por gem si falla apt
+    if ! command -v lolcat &>/dev/null; then
+        if command -v gem &>/dev/null; then
+            gem install lolcat &>/dev/null
+            if command -v lolcat &>/dev/null; then
+                echo -e "\033[97m  # gem install lolcat..................... \033[92mINSTALADO\033[0m"
+            else
+                echo -e "\033[97m  # gem install lolcat..................... \033[91mFALLO DE INSTALACION\033[0m"
+                PAQUETES_FAILED+=("lolcat")
+            fi
+        fi
+    fi
+
+    echo -e "$BARRA"
+    echo -e "\033[92m La instalación de paquetes necesarios ha finalizado"
+    echo -e "$BARRA"
+    if [ ${#PAQUETES_FAILED[@]} -gt 0 ]; then
+        echo -e "\033[91m Fallaron los siguientes paquetes: ${PAQUETES_FAILED[*]}"
+        echo -ne "\033[97m ¿Desea reintentar la instalación? [s/n]: "
+        read -r inst
+        if [[ $inst =~ ^[sSyY]$ ]]; then
+            PAQUETES_FAILED=()
+            install_packages
+        fi
+    fi
 }
 
 clear
 msg -bar2
-figlet "Bienvenido" | lolcat
+if command -v figlet &>/dev/null && command -v lolcat &>/dev/null; then
+    figlet "Bienvenido" | lolcat
+else
+    echo -e "\033[92mBienvenido\033[0m"
+fi
 echo -e "$BARRA"
 echo -e "\033[92m        -- Bienvenido al instalador V2RAY -- "
 echo -e "$BARRA"
 echo -e "\033[1;33m ¡La instalación de los paquetes ha comenzado!"
 echo -e "$BARRA"
-install_ini
+install_packages
