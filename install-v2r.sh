1ba49c852d0c50a64e87b77564b1bfc1d415b041
Cristian Aguilar <107384946+SINNOMBRE22@users.noreply.github.com>
2025-10-16 01:49:45 -0600

    Actualizar install-v2r.sh

--- install-v2r.sh
+++ install-v2r.sh
@@ -1,146 +1,284 @@
 #!/bin/bash
+# Author: Jrohy & OpenAI
+# github: https://github.com/Jrohy/multi-v2ray
 
-BARRA="\033[1;36m-----------------------------------------------------\033[0m"
-IVAR="/etc/http-instas"
-SCPT_DIR="/etc/SCRIPT"
-SCPinstal="$HOME/install"
-rm $(pwd)/$0
+# Path from where the script started
+begin_path=$(pwd)
 
-add-apt-repository universe
-apt update -y; apt upgrade -y
+# Install way, 0 for fresh install, 1 to keep V2Ray config and update
+install_way=0
 
-install_ini () {
-  clear
-  echo -e "$BARRA"
-  echo -e "\033[92m        -- INSTALANDO PAQUETES NECESARIOS -- "
-  echo -e "$BARRA"
-  #bc
-  [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install bc................... $ESTATUS "
-  #jq
-  [[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || apt-get install jq -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install jq................... $ESTATUS "
-  #curl
-  [[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install curl................. $ESTATUS "
-  #npm
-  [[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] || apt-get install npm -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "npm"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install npm.................. $ESTATUS "
-  #nodejs
-  [[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] || apt-get install nodejs -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "nodejs"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install nodejs............... $ESTATUS "
-  #socat
-  [[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] || apt-get install socat -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "socat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install socat................ $ESTATUS "
-  #netcat
-  [[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install netcat............... $ESTATUS "
-  #netcat-traditional
-  [[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || apt-get install netcat-traditional -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "netcat-traditional"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install netcat-traditional... $ESTATUS "
-  #net-tools
-  [[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || apt-get net-tools -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install net-tools............ $ESTATUS "
-  #cowsay
-  [[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || apt-get install cowsay -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "cowsay"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install cowsay............... $ESTATUS "
-  #figlet
-  [[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install figlet............... $ESTATUS "
-  #lolcat
-  apt-get install lolcat -y &>/dev/null
-  sudo gem install lolcat &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-  [[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-  echo -e "\033[97m  # apt-get install lolcat............... $ESTATUS "
+# Define operation variables, 0 for no, 1 for yes
+help=0
+remove=0
+chinese=0
 
-  echo -e "$BARRA"
-  echo -e "\033[92m La instalacion de paquetes necesarios a finalizado"
-  echo -e "$BARRA"
-  echo -e "\033[97m Si la instalacion de paquetes tiene fallas"
-  echo -ne "\033[97m Puede intentar de nuevo [s/n]: "
-  read inst
-  [[ $inst = @(s|S|y|Y) ]] && install_ini
+base_source_path="https://multi.netlify.app"
+util_path="/etc/v2ray_util/util.cfg"
+util_cfg="$base_source_path/v2ray_util/util_core/util.cfg"
+bash_completion_shell="$base_source_path/v2ray"
+clean_iptables_shell="$base_source_path/v2ray_util/global_setting/clean_iptables.sh"
+
+# Temporary disable alias for CentOS
+[[ -f /etc/redhat-release && -z $(echo $SHELL | grep zsh) ]] && unalias -a
+if [[ -z $(echo $SHELL | grep zsh) ]]; then
+    env_file=".bashrc"
+else
+    env_file=".zshrc"
+fi
+
+####### Color codes ########
+red="31m"
+green="32m"
+yellow="33m"
+blue="36m"
+fuchsia="35m"
+
+colorEcho(){
+    color=$1
+    echo -e "\033[${color}${@:2}\033[0m"
 }
 
-msg () {
-  BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
-  AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCOR='\e[0m'
-  case $1 in
-    -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
-    -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-    -verm)cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}";;
-    -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-    -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-    -bra)cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}";;
-    "-bar2"|"-bar")cor="${VERMELHO}======================================================" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
-  esac
-}
-
-ofus () {
-  unset server
-  server=$(echo ${txt_ofuscatw}|cut -d':' -f1)
-  unset txtofus
-  number=$(expr length $1)
-  for((i=1; i<$number+1; i++)); do
-    txt[$i]=$(echo "$1" | cut -b $i)
-    case ${txt[$i]} in
-      ".")txt[$i]="*";;
-      "*")txt[$i]=".";;
-      "1")txt[$i]="@";;
-      "@")txt[$i]="1";;
-      "2")txt[$i]="?";;
-      "?")txt[$i]="2";;
-      "4")txt[$i]="%";;
-      "%")txt[$i]="4";;
-      "-")txt[$i]="K";;
-      "K")txt[$i]="-";;
+####### Get Params #########
+while [[ $# > 0 ]]; do
+    key="$1"
+    case $key in
+        --remove)
+        remove=1
+        ;;
+        -h|--help)
+        help=1
+        ;;
+        -k|--keep)
+        install_way=1
+        colorEcho ${blue} "Keeping config to update\n"
+        ;;
+        --zh)
+        chinese=1
+        colorEcho ${blue} "Installing Chinese version..\n"
+        ;;
+        *)
+        # unknown option
+        ;;
     esac
-    txtofus+="${txt[$i]}"
-  done
-  echo "$txtofus" | rev
+    shift # past argument or value
+done
+
+#############################
+
+help(){
+    echo "bash v2ray.sh [-h|--help] [-k|--keep] [--remove]"
+    echo "  -h, --help           Show help"
+    echo "  -k, --keep           Keep the config.json to update"
+    echo "      --remove         Remove v2ray, xray && multi-v2ray"
+    echo "                       No params to new install"
+    return 0
 }
 
-verificar_arq () {
-  unset ARQ
-  case $1 in
-    "v2r.sh")ARQ="/usr/bin/";;
-  esac
-  mv -f ${SCPinstal}/$1 ${ARQ}/$1
-  chmod +x ${ARQ}/$1
+removeV2Ray() {
+    # Uninstall V2Ray script
+    bash <(curl -L -s https://multi.netlify.app/go.sh) --remove >/dev/null 2>&1
+    rm -rf /etc/v2ray >/dev/null 2>&1
+    rm -rf /var/log/v2ray >/dev/null 2>&1
+
+    # Uninstall Xray script
+    bash <(curl -L -s https://multi.netlify.app/go.sh) --remove -x >/dev/null 2>&1
+    rm -rf /etc/xray >/dev/null 2>&1
+    rm -rf /var/log/xray >/dev/null 2>&1
+
+    # Clean up V2Ray-related iptable rules
+    bash <(curl -L -s $clean_iptables_shell)
+
+    # Uninstall multi-v2ray
+    pip uninstall v2ray_util -y
+    rm -rf /usr/share/bash-completion/completions/v2ray.bash >/dev/null 2>&1
+    rm -rf /usr/share/bash-completion/completions/v2ray >/dev/null 2>&1
+    rm -rf /usr/share/bash-completion/completions/xray >/dev/null 2>&1
+    rm -rf /etc/bash_completion.d/v2ray.bash >/dev/null 2>&1
+    rm -rf /usr/local/bin/v2ray >/dev/null 2>&1
+    rm -rf /etc/v2ray_util >/dev/null 2>&1
+    rm -rf /etc/profile.d/iptables.sh >/dev/null 2>&1
+    rm -rf /root/.iptables >/dev/null 2>&1
+
+    # Remove V2Ray cron job
+    crontab -l | sed '/SHELL=/d;/v2ray/d' | sed '/SHELL=/d;/xray/d' > crontab.txt
+    crontab crontab.txt >/dev/null 2>&1
+    rm -f crontab.txt >/dev/null 2>&1
+
+    # Restart cron service
+    if [[ ${package_manager} == 'dnf' || ${package_manager} == 'yum' ]]; then
+        systemctl restart crond >/dev/null 2>&1
+    else
+        systemctl restart cron >/dev/null 2>&1
+    fi
+
+    # Remove multi-v2ray environment variables
+    sed -i '/v2ray/d' ~/$env_file
+    sed -i '/xray/d' ~/$env_file
+    source ~/$env_file
+
+    rc_service=$(systemctl status rc-local | grep loaded | egrep -o "[A-Za-z/]+/rc-local.service")
+    rc_file=$(cat $rc_service | grep ExecStart | awk '{print $1}' | cut -d = -f2)
+    sed -i '/iptables/d' ~/$rc_file
+
+    colorEcho ${green} "Uninstall success!"
 }
 
-install_ini
-clear
-echo -e "$BARRA"
-echo -e "\033[92m        -- INSTALANDO V2RAY -- "
-echo -e "$BARRA"
-sleep 2
-source <(curl -sL https://multi.netlify.app/v2ray.sh)
-clear
-echo -e "$BARRA"
-echo -e "\033[1;33m Perfecto, utilize el comando\n       \033[1;31mv2r.sh o v2r\n \033[1;33mpara administrar v2ray"
-echo -e "$BARRA"
-echo -ne "\033[0m"
+closeSELinux() {
+    # Disable SELinux
+    if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        setenforce 0
+    fi
+}
+
+checkSys() {
+    # Check if the user is root
+    [ $(id -u) != "0" ] && { colorEcho ${red} "Error: You must be root to run this script"; exit 1; }
+
+    if [[ `command -v apt-get` ]]; then
+        package_manager='apt-get'
+    elif [[ `command -v dnf` ]]; then
+        package_manager='dnf'
+    elif [[ `command -v yum` ]]; then
+        package_manager='yum'
+    else
+        colorEcho $red "Not supported OS!"
+        exit 1
+    fi
+}
+
+installDependent(){
+    if [[ ${package_manager} == 'dnf' || ${package_manager} == 'yum' ]]; then
+        ${package_manager} install socat crontabs bash-completion which -y
+    else
+        ${package_manager} update
+        ${package_manager} install socat cron bash-completion ntpdate gawk -y
+    fi
+
+    # Install Python3 & pip
+    source <(curl -sL https://python3.netlify.app/install.sh)
+}
+
+updateProject() {
+    [[ ! $(type pip 2>/dev/null) ]] && colorEcho $red "pip not installed!" && exit 1
+
+    [[ -e /etc/profile.d/iptables.sh ]] && rm -f /etc/profile.d/iptables.sh
+
+    rc_service=$(systemctl status rc-local | grep loaded | egrep -o "[A-Za-z/]+/rc-local.service")
+    rc_file=$(cat $rc_service | grep ExecStart | awk '{print $1}' | cut -d = -f2)
+
+    if [[ ! -e $rc_file || -z $(cat $rc_file | grep iptables) ]]; then
+        local_ip=$(curl -s http://api.ipify.org 2>/dev/null)
+        [[ $(echo $local_ip | grep :) ]] && iptable_way="ip6tables" || iptable_way="iptables"
+        if [[ ! -e $rc_file || -z $(cat $rc_file | grep "/bin/bash") ]]; then
+            echo "#!/bin/bash" >> $rc_file
+        fi
+        if [[ -z $(cat $rc_service | grep "\[Install\]") ]]; then
+            cat >> $rc_service << EOF
+
+[Install]
+WantedBy=multi-user.target
+EOF
+            systemctl daemon-reload
+        fi
+        echo "[[ -e /root/.iptables ]] && $iptable_way-restore -c < /root/.iptables" >> $rc_file
+        chmod +x $rc_file
+        systemctl restart rc-local
+        systemctl enable rc-local
+        $iptable_way-save -c > /root/.iptables
+    fi
+
+    pip install -U v2ray_util
+
+    if [[ -e $util_path ]]; then
+        [[ -z $(cat $util_path | grep lang) ]] && echo "lang=en" >> $util_path
+    else
+        mkdir -p /etc/v2ray_util
+        curl $util_cfg > $util_path
+    fi
+
+    [[ $chinese == 1 ]] && sed -i "s/lang=en/lang=zh/g" $util_path
+
+    rm -f /usr/local/bin/v2ray >/dev/null 2>&1
+    ln -s $(which v2ray-util) /usr/local/bin/v2ray
+    rm -f /usr/local/bin/xray >/dev/null 2>&1
+    ln -s $(which v2ray-util) /usr/local/bin/xray
+
+    # Remove old V2Ray bash_completion scripts
+    [[ -e /etc/bash_completion.d/v2ray.bash ]] && rm -f /etc/bash_completion.d/v2ray.bash
+    [[ -e /usr/share/bash-completion/completions/v2ray.bash ]] && rm -f /usr/share/bash-completion/completions/v2ray.bash
+
+    # Update V2Ray bash_completion script
+    curl $bash_completion_shell > /usr/share/bash-completion/completions/v2ray
+    curl $bash_completion_shell > /usr/share/bash-completion/completions/xray
+    if [[ -z $(echo $SHELL | grep zsh) ]]; then
+        source /usr/share/bash-completion/completions/v2ray
+        source /usr/share/bash-completion/completions/xray
+    fi
+    
+    # Install V2Ray main program
+    [[ ${install_way} == 0 ]] && bash <(curl -L -s https://multi.netlify.app/go.sh)
+}
+
+timeSync() {
+    if [[ ${install_way} == 0 ]]; then
+        echo -e "Time Synchronizing.. "
+        if [[ `command -v ntpdate` ]]; then
+            ntpdate pool.ntp.org
+        elif [[ `command -v chronyc` ]]; then
+            chronyc -a makestep
+        fi
+
+        if [[ $? -eq 0 ]]; then 
+            colorEcho $green "Time Sync Success"
+            colorEcho $blue "now: `date -R`"
+        fi
+    fi
+}
+
+profileInit() {
+    # Clean V2Ray module environment variables
+    [[ $(grep v2ray ~/$env_file) ]] && sed -i '/v2ray/d' ~/$env_file && source ~/$env_file
+
+    # Resolve Python3 Chinese display issue
+    [[ -z $(grep PYTHONIOENCODING=utf-8 ~/$env_file) ]] && echo "export PYTHONIOENCODING=utf-8" >> ~/$env_file && source ~/$env_file
+
+    # New configuration for fresh installation
+    [[ ${install_way} == 0 ]] && v2ray new
+
+    echo ""
+}
+
+installFinish() {
+    # Return to the original path
+    cd ${begin_path}
+
+    [[ ${install_way} == 0 ]] && WAY="install" || WAY="update"
+    colorEcho  ${green} "multi-v2ray ${WAY} success!\n"
+
+    if [[ ${install_way} == 0 ]]; then
+        clear
+        v2ray info
+        echo -e "Please input 'v2ray' command to manage V2Ray\n"
+    fi
+}
+
+
+main() {
+
+    [[ ${help} == 1 ]] && help && return
+    [[ ${remove} == 1 ]] && removeV2Ray && return
+
+    [[ ${install_way} == 0 ]] && colorEcho ${blue} "New install\n"
+
+    checkSys
+    installDependent
+    closeSELinux
+    timeSync
+    updateProject
+    profileInit
+    installFinish
+}
+
+main
